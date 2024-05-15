3333a4c96f0fbe1c6f46ff42393626396e952e2a
Pazhanisamy <pazhanisamy3434@gmail.com>
2024-05-15 11:43:32 +0530

    code clean up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
